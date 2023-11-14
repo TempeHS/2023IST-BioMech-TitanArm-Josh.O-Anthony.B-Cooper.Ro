949dd192e574a86926bd0c91a93cbf2cac6f1e71
joshozeri <124547387+joshozeri@users.noreply.github.com>
2023-11-15 09:34:49 +1100

    update

--- Servo_motor_code/Servo_motor_code.ino
+++ Servo_motor_code/Servo_motor_code.ino
@@ -21,5 +21,5 @@ void loop() {
    val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15); 
+  delay(20); 
 }
